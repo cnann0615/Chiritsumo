5651ac00e9a1e230d8a8f67d7a10b1fa22665019
cnann0615 <leave.it.to.7010@gmail.com>
2024-10-22 23:30:44 +0900

    1022 ログイン周りの実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
-import { api, HydrateClient } from "~/trpc/server";
+import { HydrateClient } from "~/trpc/server";
 import { getServerAuthSession } from "~/server/auth";
 import SignIn from "./_components/SignIn";
 import SignOut from "./_components/SignOut";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-
-  void api.post.getLatest.prefetch();
-
   const session = await getServerAuthSession();
-
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
