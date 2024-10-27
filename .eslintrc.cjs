004c30d6b127fa0802a7d6e3039a299a4032e61f
cnann0615 <leave.it.to.7010@gmail.com>
2024-10-27 23:52:04 +0900

    1027 デプロイ

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,30 +11,14 @@ const config = {
     "plugin:@typescript-eslint/stylistic-type-checked",
   ],
   rules: {
-    // "@typescript-eslint/array-type": "off",
-    // "@typescript-eslint/consistent-type-definitions": "off",
-    // "@typescript-eslint/consistent-type-imports": [
-    //   "warn",
-    //   {
-    //     prefer: "type-imports",
-    //     fixStyle: "inline-type-imports",
-    //   },
-    // ],
-    // "@typescript-eslint/no-unused-vars": [
-    //   "warn",
-    //   {
-    //     argsIgnorePattern: "^_",
-    //   },
-    // ],
-    // "@typescript-eslint/require-await": "off",
-    // "@typescript-eslint/no-misused-promises": [
-    //   "error",
-    //   {
-    //     checksVoidReturn: {
-    //       attributes: false,
-    //     },
-    //   },
-    // ],
+    // ESLintのルールを無効化
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-unnecessary-type-assertion": "off",
+    "@typescript-eslint/consistent-type-imports": "off",
+    "@typescript-eslint/array-type": "off",
+    "@typescript-eslint/consistent-type-definitions": "off",
+    "@typescript-eslint/require-await": "off",
+    "@typescript-eslint/no-misused-promises": "off",
   },
 };
 module.exports = config;
