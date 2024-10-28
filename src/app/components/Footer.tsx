f2f46a74153c11ede0a3fb1c2fd22e615c1a01ec
cnann0615 <leave.it.to.7010@gmail.com>
2024-10-28 23:53:10 +0900

    1028 fix

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,24 +1,13 @@
-import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
   return (
     <footer className="bg-gray-800 px-[4%] py-6 text-white">
-      <div className="container mx-auto flex flex-col items-center gap-4 text-center sm:flex-row sm:justify-between sm:text-left">
-        {/* 利用規約やプライバシーポリシー */}
-        <div className="flex flex-col items-center gap-4 text-sm text-gray-400 sm:flex-row sm:gap-8">
-          <Link href="/terms" className="hover:text-white">
-            利用規約
-          </Link>
-          <Link href="/privacy" className="hover:text-white">
-            プライバシーポリシー
-          </Link>
+      <div className="container mx-auto text-center">
+        {/* コピーライト */}
+        <div className="text-sm text-gray-500">
+          &copy; {new Date().getFullYear()} ちりつも. All Rights Reserved.
         </div>
-      </div>
-
-      {/* コピーライト */}
-      <div className="mt-4 text-center text-sm text-gray-500">
-        &copy; {new Date().getFullYear()} ちりつも. All Rights Reserved.
       </div>
     </footer>
   );
