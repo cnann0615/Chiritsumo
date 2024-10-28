b0fcca616c5e99ef750a1c2b99cb1999055741d7
cnann0615 <leave.it.to.7010@gmail.com>
2024-10-28 22:25:28 +0900

    1028 デプロイ

--- src/app/main/Main.tsx
+++ src/app/main/Main.tsx
@@ -8,21 +8,22 @@ import TsumoBalanceProgress from "./TsumoBalanceProgress";
 import { db } from "~/server/db";
 
 const Main = async () => {
-  const session = await getServerAuthSession();
+  try {
+    const session = await getServerAuthSession();
 
-  const existingRecord = await db.tsumoBalance.findUnique({
-    where: { userId: session!.user.id },
-  });
-  if (!existingRecord) {
-    await db.tsumoBalance.create({
-      data: {
-        userId: session!.user.id,
-        tsumoBalance: 0,
-      },
+    const existingRecord = await db.tsumoBalance.findUnique({
+      where: { userId: session!.user.id },
     });
-    console.log("New TsumoBalance record created successfully");
-  } else {
-    console.log("TsumoBalance record already exists for userId:");
+    if (!existingRecord) {
+      await db.tsumoBalance.create({
+        data: {
+          userId: session!.user.id,
+          tsumoBalance: 0,
+        },
+      });
+    }
+  } catch (error) {
+    console.error(error);
   }
 
   return (
