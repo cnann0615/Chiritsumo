fc517092af5c6260479d32adf5945f8f9ec88271
cnann0615 <leave.it.to.7010@gmail.com>
2024-11-03 22:30:03 +0900

    1103 トップ画面のローディング中の表示を数字のアニメーションから、円くるくるに変更

--- src/app/main/BalanceDisplay.tsx
+++ src/app/main/BalanceDisplay.tsx
@@ -8,24 +8,35 @@ const BalanceDisplay = () => {
   const { data: balance, isLoading } = api.balance.read.useQuery();
 
   // 残高を取得中にランダムな数字をシャッフルして表示するアニメーションを表示
-  const [randomNumber, setRandomNumber] = useState(0);
-  useEffect(() => {
-    if (isLoading) {
-      const intervalId = setInterval(() => {
-        setRandomNumber(Math.floor(Math.random() * 90 + 10));
-      }, 100); // 100ミリ秒ごとにランダムな2桁の数字を生成
-      return () => clearInterval(intervalId);
-    }
-  }, [isLoading]);
+  // const [randomNumber, setRandomNumber] = useState(0);
+  // useEffect(() => {
+  //   if (isLoading) {
+  //     const intervalId = setInterval(() => {
+  //       setRandomNumber(Math.floor(Math.random() * 90 + 10));
+  //     }, 50); // 100ミリ秒ごとにランダムな2桁の数字を生成
+  //     return () => clearInterval(intervalId);
+  //   }
+  // }, [isLoading]);
 
   return (
     <div className="mb-16 mt-32 flex flex-col items-center text-2xl">
       <h3 className="mb-4 text-2xl font-bold sm:mb-6 sm:text-4xl">残高</h3>
       <div className="mb-2 text-[100px] font-bold leading-none sm:mb-4 sm:text-[150px]">
         {/* メインの金額表示 */}
-        {isLoading ? randomNumber : balance?.balance}
+        {isLoading ? (
+          <div
+            className="flex h-32 w-32 justify-center sm:h-40 sm:w-40"
+            aria-label="読み込み中"
+          >
+            <div className="mt-3 h-20 w-20 animate-spin rounded-full border-4 border-pink-500 border-t-transparent sm:mt-4 sm:h-28 sm:w-28"></div>
+          </div>
+        ) : (
+          <div className="flex h-32 w-32 justify-center sm:h-40 sm:w-40">
+            {balance?.balance}
+          </div>
+        )}
       </div>
-      <div className="text-3xl font-bold sm:text-5xl">JPY</div>
+      <div className="text-4xl font-bold sm:text-5xl">JPY</div>
     </div>
   );
 };
